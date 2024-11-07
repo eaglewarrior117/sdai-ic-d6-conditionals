56dffe35ee500800f88b948cd809bd8f2839c161
Reynaldo Reyes <reythe6reat@gmail.com>
2024-11-07 05:05:37 +0000

    README.md

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // all code for this lab to be completed in this file
+let currentTemperature = 32;
+
+if (currentTemperature > 32) {
+    console.log("It is not freezing")
+} else {
+    console.log("It is freezing")
+}
+
+let age1 = "eleven"
+let age2 = 32
+
+if (age1 > age2) {
+    console.log("Hey I'm Old!")
+} else if (age1 < age2) {
+    console.log("DAMM")
+} else if (age1===age2){
+    //equal age
+    console.log("Sweet!")
+} else {
+    console.log("Dude, try again!")
+}
+
+//'isLoggedIn'section
+
+let  isLoggedIn = "true"
+
+if (isLoggedIn === true) {
+    console.log("Hello!") //rep log in
+} else {
+    console.log("HAHA, Try again") //not logged in
+}
+
